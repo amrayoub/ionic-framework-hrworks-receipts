aa1256d49444522fe768edc359384e2fd682118b
christoph-hummel <christoph.hummel@hrworks.de>
2014-06-18 12:53:40 +0200

    fix

--- HRworksReceipts/www/js/controllers.js
+++ HRworksReceipts/www/js/controllers.js
@@ -1,4 +1,33 @@
-angular.module('starter.controllers', [])
+angular.module('starter.controllers', ['ionic'])
+
+.controller('addReceiptCtrl', function($scope, $localstorage, $state) {
+	$scope.form = {};
+	$scope.receiptKinds = $localstorage.getObject('receiptKinds');
+	generateGUID = function(){
+		var guid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+			var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+			return v.toString(16);
+		});    
+		return guid;
+	};
+	$scope.createReceipt = function () {
+		console.log($scope.form);
+		console.log($scope);
+		$localstorage.insertObject('receipts', {
+			text : 'Beleg',
+			amount : 123,
+			date : new Date(),
+			receiptKind : "1",
+			kindOfPayment : "1",
+			currency : "EUR",
+			rimestamp : new Date(),
+			guid: generateGUID()
+		});
+		if($scope.$viewHistory.backView != null){
+			$scope.$viewHistory.backView.go();
+		}
+	};
+})
 
 .controller('receiptsCtrl', function($scope, $localstorage) {
 	$scope.receipts = $localstorage.getObject('receipts');
@@ -20,32 +49,7 @@ angular.module('starter.controllers', [])
 .controller('infosCtrl', function($scope) {
 })
 
-.controller('addReceiptCtrl', function($scope, $localstorage, $state) {
-	$scope.form = {};
-	$scope.receiptKinds = $localstorage.getObject('receiptKinds');
-	/*generateGUID = function(){
-		var guid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-			var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
-			return v.toString(16);
-		});    
-		return guid;
-	};*/
-	$scope.createReceipt = function () {
-		console.log($scope.form.text);
-		console.log($scope);
-		$localstorage.insertObject('receipts', {
-			text : 'Beleg',
-			amount : 123,
-			date : new Date(),
-			receiptKind : "1",
-			kindOfPayment : "1",
-			currency : "EUR",
-			rimestamp : new Date(),
-			guid: 'xx'
-		});
-		$scope.$viewHistory.backView.go();
-	}
-})
+
 
 .controller('updateReceiptCtrl', function($scope, $localstorage, $stateParams, Friends) {
   $scope.friend = $localstorage.get($stateParams.friendId);
