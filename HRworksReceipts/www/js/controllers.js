105fe2fd560c116034a9392d4f9286fca4840a63
christoph-hummel <christoph.hummel@hrworks.de>
2014-06-18 12:29:46 +0200

    tvg

--- HRworksReceipts/www/js/controllers.js
+++ HRworksReceipts/www/js/controllers.js
@@ -21,15 +21,18 @@ angular.module('starter.controllers', [])
 })
 
 .controller('addReceiptCtrl', function($scope, $localstorage, $state) {
+	$scope.form = {};
 	$scope.receiptKinds = $localstorage.getObject('receiptKinds');
-	generateGUID = function(){
+	/*generateGUID = function(){
 		var guid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
 			var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
 			return v.toString(16);
 		});    
 		return guid;
-	};
-	$scope.save = function () {
+	};*/
+	$scope.createReceipt = function () {
+		console.log($scope.form.text);
+		console.log($scope);
 		$localstorage.insertObject('receipts', {
 			text : 'Beleg',
 			amount : 123,
@@ -38,10 +41,9 @@ angular.module('starter.controllers', [])
 			kindOfPayment : "1",
 			currency : "EUR",
 			rimestamp : new Date(),
-			guid: generateGUID()
+			guid: 'xx'
 		});
 		$scope.$viewHistory.backView.go();
-
 	}
 })
 
