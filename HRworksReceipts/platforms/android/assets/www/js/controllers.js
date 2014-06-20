6cae065e5c9a70b019965552ef40ae5ba5ad8446
christoph-hummel <christoph.hummel@hrworks.de>
2014-06-20 16:24:12 +0200

    update addReceipt

--- HRworksReceipts/platforms/android/assets/www/js/controllers.js
+++ HRworksReceipts/platforms/android/assets/www/js/controllers.js
@@ -1,8 +1,16 @@
 angular.module('starter.controllers', ['ionic'])
 
-.controller('addReceiptCtrl', function($scope, $localstorage, $state) {
+.controller('addReceiptCtrl', function($scope, $localstorage, $filter, $state) {
 	$scope.form = {};
+	$scope.form.date = $filter('date')(new Date(),'shortDate');
+	console.log($scope.form);
 	$scope.receiptKinds = $localstorage.getObject('receiptKinds');
+	$scope.receiptKindsSelected = $scope.receiptKinds[0];
+	$scope.kindsOfPayment = $localstorage.getObject('kindsOfPayment');
+	$scope.kindsOfPaymentSelected = $scope.kindsOfPayment[1];
+	$scope.currencies = $localstorage.getObject('currencies');
+	$scope.currenciesSelected = $scope.currencies[33];
+	console.log($scope.currenciesSelected.symbol);
 	generateGUID = function(){
 		var guid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
 			var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
@@ -31,7 +39,7 @@ angular.module('starter.controllers', ['ionic'])
 				receiptKind : "1",
 				kindOfPayment : "1",
 				currency : "EUR",
-				rimestamp : new Date(),
+				timestamp : Date(),
 				guid: generateGUID()
 			});
 			if($scope.$viewHistory.backView != null){
@@ -41,21 +49,54 @@ angular.module('starter.controllers', ['ionic'])
 	};
 })
 
-.controller('receiptsCtrl', function($scope, $localstorage) {
+.controller('receiptsCtrl', function($scope, $localstorage,$ionicLoading) {
+
 	$scope.receipts = $localstorage.getObject('receipts');
+	$scope.getItemHeight = function(item, index) {
+		return (index % 1) === 0 ? 50 : 50;
+	};
+	$scope.getItemWidth = function(item) {
+		return '100%';
+	};
+	$scope.show = function() {
+		$ionicLoading.show({
+			template: 'Synchronisieren...',
+			duration: '1000'
+		});
+	};
+	$scope.hide = function(){
+		$ionicLoading.hide();
+	};
 })
 
-.controller('FriendDetailCtrl', function($scope, $stateParams) {
-  $scope.receipts = Friends.get($stateParams.friendId);
-})
-
-.controller('AccountCtrl', function($scope) {
+.controller('accountCtrl', function($scope) {
 })
 
 .controller('feedbackCtrl', function($scope) {
 })
 
-.controller('settingsCtrl', function($scope) {
+.controller('settingsCtrl', function($scope, $localstorage) {
+	generateGUID = function(){
+		var guid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+			var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+			return v.toString(16);
+		});    
+		return guid;
+	};
+	$scope.create100Receipts = function() {
+		for(var i= 0; i < 100; i++) {
+			$localstorage.insertObject('receipts', {
+				text : 'Beleg' + i,
+				amount : 123,
+				date : '20120304',
+				receiptKind : '1',
+				kindOfPayment : '1',
+				currency : "EUR",
+				timestamp : Date(),
+				guid: generateGUID()
+			});
+		}
+	};
 })
 
 .controller('infosCtrl', function($scope) {
@@ -64,5 +105,5 @@ angular.module('starter.controllers', ['ionic'])
 
 
 .controller('updateReceiptCtrl', function($scope, $localstorage, $stateParams, Friends) {
-  $scope.friend = $localstorage.get($stateParams.friendId);
+	$scope.friend = $localstorage.get($stateParams.friendId);
 });
