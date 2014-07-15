3a07fbd1a09f6aab4e632b770bdc8a2bcbb0380c
christoph-hummel <christoph.hummel@hrworks.de>
2014-07-15 13:56:43 +0200

    fixed error
    This is a fix for the version : 2 error

--- HRworksReceipts/www/js/services.js
+++ HRworksReceipts/www/js/services.js
@@ -131,7 +131,7 @@ angular.module('starter.services', [])
 		jsonObject.personId = userData.personId;
 		jsonObject.dateAndTime = (new Date()).toISO8601();
 		jsonObject.mobileApplicationAuthorization = "HRworksMobileApp";
-		jsonObject.deviceId = "1";//$cordovaDevice.getUUID();
+		jsonObject.deviceId = $cordovaDevice.getUUID();
 		jsonObject.languageKey = $translate.use();
 		jsonObject.version = "1";
 		jsonObject.signature = generateSignature(jsonObject.companyId, jsonObject.personId, request, jsonObject.dateAndTime, userData.mobilePassword);
@@ -185,8 +185,6 @@ angular.module('starter.services', [])
 					},500)
 				});
 			}).error(function(data, status, headers, config) {
-				$localstorage.setObject('version', {
-					version : 2
 				});
 			});
 		},
@@ -199,8 +197,8 @@ angular.module('starter.services', [])
 			jsonObject.personId = user.personId;
 			jsonObject.dateAndTime = (new Date()).toISO8601();
 			jsonObject.mobileApplicationAuthorization = "HRworksMobileApp";
-			jsonObject.deviceId = "1";//$cordovaDevice.getUUID();
-			jsonObject.deviceName = "pc";//$cordovaDevice.getModel();
+			jsonObject.deviceId = $cordovaDevice.getUUID();
+			jsonObject.deviceName = $cordovaDevice.getModel();
 			jsonObject.languageKey = $translate.use();
 			jsonObject.version = "1";
 			jsonObject.signature = generateSignature(jsonObject.companyId, jsonObject.personId, request, jsonObject.dateAndTime, user.mobilePassword);
